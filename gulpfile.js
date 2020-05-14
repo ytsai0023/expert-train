41b5c76112ecfd69498751e49dc1a6355b2769a8
PhoebeTsai <ysai.pro@gmail.com>
2020-05-15 08:42:25 +0900

    rwd completed

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const watch = require('gulp-watch');
 const browserSync = require('browser-sync');
+const template = require('gulp-template');
+const data = require('gulp-data');
+var ejs = require('gulp-ejs')
+var log = require('fancy-log')
+const {header,footer,links}= require("./source/template/component/components");
 const reload = browserSync.reload;
+
+
 
 
 // gulp.task('sass',function(){
@@ -21,6 +28,21 @@ gulp.task('sass',function(){
     })
 })
 
+
+gulp.task('template',function(){
+    return watch('./source/template/**/*.html',function(){
+        gulp.src(['./source/template/**/*.html','./source/template/**/*.js'])
+        .pipe(template({
+            header
+           ,footer
+           ,links
+        }))
+        .pipe(gulp.dest('./public'))
+        .pipe(browserSync.stream())
+    })
+
+})
+
 gulp.task('browser-sync',function (){
     browserSync.init({
         server:'./public',
@@ -29,7 +51,8 @@ gulp.task('browser-sync',function (){
     })
 })
 
-gulp.task('default',gulp.parallel('sass','browser-sync'))
+gulp.task('default',gulp.parallel('sass','template','browser-sync'))
+
 
 
 
