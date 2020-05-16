4bfe682dff49a1f910b6a19307492dcd7661a4fa
PhoebeTsai <ysai.pro@gmail.com>
2020-05-17 06:05:14 +0900

    Add config of nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,9 @@ const sass = require('gulp-sass');
 const watch = require('gulp-watch');
 const browserSync = require('browser-sync');
 const template = require('gulp-template');
-const data = require('gulp-data');
-var ejs = require('gulp-ejs')
-var log = require('fancy-log')
+var ejs = require('gulp-ejs');
+const rename = require('gulp-rename');
+var nunjucksRender = require('gulp-nunjucks-render');
 const {header,footer,links}= require("./source/template/component/components");
 const reload = browserSync.reload;
 
@@ -22,7 +22,7 @@ gulp.task('sass',function(){
 
 
 gulp.task('template',function(){
-    return watch('./source/template/**/*.html',function(){
+    return watch(['./source/template/**/*.html'],function(){
         gulp.src(['./source/template/**/*.html','./source/template/**/*.js'])
         .pipe(template({
             header
@@ -35,6 +35,20 @@ gulp.task('template',function(){
 
 })
 
+gulp.task('nunjucks',function(){
+   
+   return  watch(['./source/template/**/*.html'],function(){
+    gulp.src('./source/template/**/*.html')
+    .pipe(nunjucksRender({
+        path: ['./source/template/weekFour/','./source/templates/_layout.weekFour.html'],
+        watch: false,
+      }))
+    .pipe(gulp.dest('./public'))
+    .pipe(browserSync.stream())
+    })
+    
+})
+
 gulp.task('browser-sync',function (){
     browserSync.init({
         server:'./public',
@@ -43,7 +57,7 @@ gulp.task('browser-sync',function (){
     })
 })
 
-gulp.task('default',gulp.parallel('sass','template','browser-sync'))
+gulp.task('default',gulp.parallel('sass','template','nunjucks','browser-sync'))
 
 
 
